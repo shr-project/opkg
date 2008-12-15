c4e896ceda14cb1515625b020c705b521f0e306d
ticktock35 <ticktock35@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2008-12-15 05:10:21 +0000

    opkg: add update packages lists call to libopkg_test
    git-svn-id: http://opkg.googlecode.com/svn/trunk@76 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- tests/libopkg_test.c
+++ tests/libopkg_test.c
@@ -1,15 +1,19 @@
 #include <opkg.h>
-
+#include <stdlib.h>
 
 int
 main (int argc, char **argv)
 {
   opkg_t *opkg;
+  int err;
   
   opkg = opkg_new ();
 
   opkg_set_option (opkg, "offline_root", "/tmp/");
 
+  err = opkg_update_package_lists (opkg);
+
+  printf ("opkg_update_package_lists returned %d\n", err);
 
   opkg_free (opkg);
 }
