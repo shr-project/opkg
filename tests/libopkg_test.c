d1f5423230ce4b81db9c4433399c71065506dd82
ticktock35 <ticktock35@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2008-12-15 05:12:58 +0000

    opkg: Add upgrade and upgrade_all functions to libopkg_test       Improve progress display in libopkg_test
    git-svn-id: http://opkg.googlecode.com/svn/trunk@89 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- tests/libopkg_test.c
+++ tests/libopkg_test.c
@@ -5,7 +5,8 @@
 void
 progress_callback (opkg_t *opkg, int percent, void *data)
 {
-  printf ("%s %d\n", (char*) data, percent);
+  printf ("\r%s %3d%%", (char*) data, percent);
+  fflush (stdout);
 }
 
 
@@ -22,16 +23,19 @@ main (int argc, char **argv)
   opkg_read_config_files (opkg);
 
   err = opkg_update_package_lists (opkg, progress_callback, "Updating...");
-
-  printf ("opkg_update_package_lists returned %d\n", err);
+  printf ("\nopkg_update_package_lists returned %d\n", err);
 
   err = opkg_install_package (opkg, "aspell", progress_callback, "Installing...");
+  printf ("\nopkg_install_package returned %d\n", err);
 
-  printf ("opkg_install_package returned %d\n", err);
+  err = opkg_upgrade_package (opkg, "aspell", progress_callback, "Upgrading...");
+  printf ("\nopkg_upgrade_package returned %d\n", err);
+
+  err = opkg_upgrade_all (opkg, progress_callback, "Upgrading all...");
+  printf ("\nopkg_upgrade_package returned %d\n", err);
 
   err = opkg_remove_package (opkg, "aspell", progress_callback, "Removing...");
-
-  printf ("opkg_remove_package returned %d\n", err);
+  printf ("\nopkg_remove_package returned %d\n", err);
 
   opkg_free (opkg);
 }
