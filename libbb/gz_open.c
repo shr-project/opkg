47daab916286d7ac837ecaadb607b09797822fc2
ticktock35 <ticktock35@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2008-12-27 16:52:36 +0000

    Thanks for  Mike Westerhof <mwester@dls.net> ----------  This patchset updates the libbb stuff to use a vfork() version of  gz_open, called gzvopen.  This is done because a standard fork will  duplicate the entire address space.  This will invoke the OOM  (out of memory) killer on small-memory machines, because most often  by the time we unzip any package, we've read the entire package  database into memory already.  By using vfork() and immediatly  execing the external gunzip utility, we avoid the need to clone the  entire address space.
     Yes, this is actually **LESS** efficient than the original way!
     But there is no way to (currently) dodge the OOM killer on a
     per-process basis, so the alternatives are to either change the
     OOM killer behavior system-wide, or to use this workaround.
    
     Mike Westerhof, Dec 2008
    
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@190 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- libbb/gz_open.c
+++ libbb/gz_open.c
@@ -56,3 +56,78 @@ extern FILE *gz_open(FILE *compressed_file, int *pid)
 	}
 	return(fdopen(unzip_pipe[0], "r"));
 }
+
+/* gz_open implementation using gunzip and a vfork/exec -- dodges OOM killer */
+extern FILE *gzvopen(FILE *compressed_file, int *pid)
+{
+	int unzip_pipe[2];
+	off_t floc;
+	int cfile;
+
+	/* create a new file descriptor for the input stream
+	 * (it *must* be associated with a file)
+	 * and seek to the same position in that fd as the stream.
+	 */
+	cfile = dup(fileno(compressed_file));
+	floc = ftello(compressed_file);
+	lseek(cfile, floc, SEEK_SET);
+
+	/* create the pipe */
+	if (pipe(unzip_pipe)!=0) {
+		error_msg("gzvopen(): pipe error");
+		return(NULL);
+	}
+
+	*pid = vfork();
+
+	if (*pid < 0) {
+		error_msg("gzvopen(): fork failed");
+		return(NULL);
+	}
+
+	if (*pid==0) {
+		/* child process - reads STDIN, writes to pipe */
+
+		/* close unused read end of pipe */
+		close(unzip_pipe[0]);
+
+		/* connect child's stdout to the pipe write end */
+		dup2(unzip_pipe[1], 1);
+
+		/* connect child's stdin to the fd passed in to us */
+		dup2(cfile, 0);
+
+		/* execute the gunzip utility */
+		execlp("gunzip","gunzip",NULL);
+
+		/* if we get here, we had a failure - since we are
+		 * using vfork(), we cannot call exit(), must call _exit().
+		 */
+		_exit(-1);
+	}
+
+	/* Parent process is executing here */
+
+	/* we have no more need of the duplicate fd */
+	close(cfile);
+
+	/* close the write end of the pipe */
+	close(unzip_pipe[1]);
+
+	/* return the read end of the pipe as a FILE */
+	return(fdopen(unzip_pipe[0], "r"));
+}
+
+extern void gzvclose(int gunzip_pid)
+{
+        if (kill(gunzip_pid, SIGTERM) == -1) {
+		perror("gzvclose()");
+                fprintf(stderr,"%s: unable to kill gunzip pid.\n",
+			__FUNCTION__);
+        }
+
+        if (waitpid(gunzip_pid, NULL, 0) == -1) {
+                fprintf(stderr,"%s: unable to wait on gunzip pid.\n",
+			__FUNCTION__);
+        }
+}
