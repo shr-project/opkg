7bf0f50d8bc7245b0e89bd5b3c84d84d1c3629c0
ticktock35 <ticktock35@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2008-12-28 08:06:35 +0000

    opkg the config file dir to that one actually works
    git-svn-id: http://opkg.googlecode.com/svn/trunk@194 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- libopkg/args.h
+++ libopkg/args.h
@@ -47,7 +47,7 @@ struct args
 };
 typedef struct args args_t;
 
-#define ARGS_DEFAULT_CONF_FILE_DIR "/etc"
+#define ARGS_DEFAULT_CONF_FILE_DIR "/etc/opkg"
 #define ARGS_DEFAULT_CONF_FILE_NAME "opkg.conf"
 #define ARGS_DEFAULT_DEST NULL
 #define ARGS_DEFAULT_FORCE_DEFAULTS 0
