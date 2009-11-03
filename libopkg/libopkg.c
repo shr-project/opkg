99caa6bb08baa6762a09547844277f77848b76c8
graham.gower <graham.gower@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2009-11-03 06:26:31 +0000

    The list-installed command does not exist.
    git-svn-id: http://opkg.googlecode.com/svn/trunk@250 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- libopkg/libopkg.c
+++ libopkg/libopkg.c
@@ -65,7 +65,6 @@ opkg_op (int argc, char *argv[])
              !strcmp(cmd_name,"compare_versions") ||
              !strcmp(cmd_name,"compare-versions") ||
              !strcmp(cmd_name,"list_installed") ||
-             !strcmp(cmd_name,"list-installed") ||
              !strcmp(cmd_name,"status") )
            args.noreadfeedsfile = 1;
 
