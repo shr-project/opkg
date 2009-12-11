e19ce4d44b10a45f84e9c994ba3ddc81698a2cd9
graham.gower <graham.gower@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2009-12-11 04:35:57 +0000

    Rewrite the error paths.
    git-svn-id: http://opkg.googlecode.com/svn/trunk@486 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- libopkg/opkg_conf.c
+++ libopkg/opkg_conf.c
@@ -1,7 +1,8 @@
 /* opkg_conf.c - the opkg package management system
 
-   Carl D. Worth
+   Copyright (C) 2009 Ubiq Technologies <graham.gower@gmail.com>
 
+   Carl D. Worth
    Copyright (C) 2001 University of Southern California
 
    This program is free software; you can redistribute it and/or
@@ -33,6 +34,7 @@
 #include <glob.h>
 
 static int lock_fd;
+char *lock_file;
 
 static opkg_conf_t _conf;
 opkg_conf_t *conf = &_conf;
@@ -125,6 +127,7 @@ static int
 opkg_conf_set_option(const char *name, const char *value)
 {
      int i = 0;
+
      while (options[i].name) {
 	  if (strcmp(options[i].name, name) == 0) {
 	       switch (options[i].type) {
@@ -180,7 +183,8 @@ opkg_conf_parse_file(const char *filename,
 				pkg_src_list_t *pkg_src_list,
 				nv_pair_list_t *tmp_dest_nv_pair_list)
 {
-     int err;
+     int line_num = 0;
+     int err = 0;
      FILE *file;
      regex_t valid_line_re, comment_re;
 #define regmatch_size 12
@@ -189,7 +193,8 @@ opkg_conf_parse_file(const char *filename,
      file = fopen(filename, "r");
      if (file == NULL) {
 	  opkg_perror(ERROR, "Failed to open %s", filename);
-	  return -1;
+	  err = -1;
+	  goto err0;
      }
 
      opkg_msg(INFO, "Loading conf file %s.\n", filename);
@@ -197,28 +202,30 @@ opkg_conf_parse_file(const char *filename,
      err = xregcomp(&comment_re, 
 		    "^[[:space:]]*(#.*|[[:space:]]*)$",
 		    REG_EXTENDED);
-     if (err) {
-	  return -1;
-     }
-     err = xregcomp(&valid_line_re, "^[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))([[:space:]]+([^[:space:]]+))?[[:space:]]*$", REG_EXTENDED);
-     if (err) {
-	  return -1;
-     }
+     if (err)
+          goto err1;
+
+     err = xregcomp(&valid_line_re,
+		     "^[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))"
+		     "[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))"
+		     "[[:space:]]*(\"([^\"]*)\"|([^[:space:]]*))"
+		     "([[:space:]]+([^[:space:]]+))?[[:space:]]*$",
+		     REG_EXTENDED);
+     if (err)
+	  goto err2;
 
      while(1) {
-	  int line_num = 0;
 	  char *line;
 	  char *type, *name, *value, *extra;
 
-	  line = file_read_line_alloc(file);
 	  line_num++;
-	  if (line == NULL) {
-	       break;
-	  }
 
-	  if (regexec(&comment_re, line, 0, 0, 0) == 0) {
+	  line = file_read_line_alloc(file);
+	  if (line == NULL)
+	       break;
+
+	  if (regexec(&comment_re, line, 0, 0, 0) == 0)
 	       goto NEXT_LINE;
-	  }
 
 	  if (regexec(&valid_line_re, line, regmatch_size, regmatch, 0) == REG_NOMATCH) {
 	       opkg_msg(ERROR, "%s:%d: Ignoring invalid line: `%s'\n",
@@ -234,6 +241,7 @@ opkg_conf_parse_file(const char *filename,
 	       type = xstrndup(line + regmatch[3].rm_so,
 			      regmatch[3].rm_eo - regmatch[3].rm_so);
 	  }
+
 	  if (regmatch[5].rm_so > 0) {
 	       name = xstrndup(line + regmatch[5].rm_so,
 			      regmatch[5].rm_eo - regmatch[5].rm_so);
@@ -241,6 +249,7 @@ opkg_conf_parse_file(const char *filename,
 	       name = xstrndup(line + regmatch[6].rm_so,
 			      regmatch[6].rm_eo - regmatch[6].rm_so);
 	  }
+
 	  if (regmatch[8].rm_so > 0) {
 	       value = xstrndup(line + regmatch[8].rm_so,
 			       regmatch[8].rm_eo - regmatch[8].rm_so);
@@ -248,6 +257,7 @@ opkg_conf_parse_file(const char *filename,
 	       value = xstrndup(line + regmatch[9].rm_so,
 			       regmatch[9].rm_eo - regmatch[9].rm_so);
 	  }
+
 	  extra = NULL;
 	  if (regmatch[11].rm_so > 0) {
 	       extra = xstrndup (line + regmatch[11].rm_so,
@@ -289,26 +299,30 @@ opkg_conf_parse_file(const char *filename,
 	       }
 	       nv_pair_list_append(&conf->arch_list, name, value);
 	  } else {
-	       opkg_msg(ERROR, "Ignoring unknown configuration "
-		       "parameter: %s %s %s\n", type, name, value);
-	       return -1;
+	       opkg_msg(ERROR, "%s:%d: Ignoring invalid line: `%s'\n",
+		       filename, line_num, line);
 	  }
 
 	  free(type);
 	  free(name);
 	  free(value);
 	  if (extra)
-	       free (extra);
+	       free(extra);
 
-     NEXT_LINE:
+NEXT_LINE:
 	  free(line);
      }
 
-     regfree(&comment_re);
      regfree(&valid_line_re);
-     fclose(file);
-
-     return 0;
+err2:
+     regfree(&comment_re);
+err1:
+     if (fclose(file) == EOF) {
+          opkg_perror(ERROR, "Couldn't close %s", filename);
+	  err = -1;
+     }
+err0:
+     return err;
 }
 
 int
@@ -359,7 +373,10 @@ opkg_conf_write_status_files(void)
 
      list_for_each_entry(iter, &conf->pkg_dest_list.head, node) {
           dest = (pkg_dest_t *)iter->data;
-          fclose(dest->status_fp);
+          if (fclose(dest->status_fp) == EOF) {
+               opkg_perror(ERROR, "Couldn't close %s", dest->status_file_name);
+	       ret = -1;
+          }
      }
 
      return ret;
@@ -378,138 +395,179 @@ root_filename_alloc(char *filename)
 int
 opkg_conf_init(void)
 {
-     int err;
-     char *tmp_dir_base, *tmp2;
-     nv_pair_list_t tmp_dest_nv_pair_list;
-     char *lock_file = NULL;
-     glob_t globbuf;
-     char *etc_opkg_conf_pattern;
+	int i;
+	char *tmp, *tmp_dir_base, **tmp_val;
+	nv_pair_list_t tmp_dest_nv_pair_list;
+	char *lock_file = NULL;
+	glob_t globbuf;
+	char *etc_opkg_conf_pattern;
 
-     conf->restrict_to_default_dest = 0;
-     conf->default_dest = NULL;
+	conf->restrict_to_default_dest = 0;
+	conf->default_dest = NULL;
 #if defined(HAVE_PATHFINDER)
-     conf->check_x509_path = 1;
+	conf->check_x509_path = 1;
 #endif
 
-     pkg_src_list_init(&conf->pkg_src_list);
+	pkg_src_list_init(&conf->pkg_src_list);
+	pkg_dest_list_init(&conf->pkg_dest_list);
+	nv_pair_list_init(&conf->arch_list);
+	nv_pair_list_init(&tmp_dest_nv_pair_list);
 
-     nv_pair_list_init(&tmp_dest_nv_pair_list);
-     pkg_dest_list_init(&conf->pkg_dest_list);
+	if (!conf->offline_root)
+		conf->offline_root = xstrdup(getenv("OFFLINE_ROOT"));
 
-     nv_pair_list_init(&conf->arch_list);
+	if (conf->conf_file) {
+		struct stat st;
+		if (stat(conf->conf_file, &st) == -1) {
+			opkg_perror(ERROR, "Couldn't stat %s", conf->conf_file);
+			goto err0;
+		}
+		if (opkg_conf_parse_file(conf->conf_file,
+				&conf->pkg_src_list, &tmp_dest_nv_pair_list))
+			goto err1;
+	}
 
-     if (!conf->offline_root)
-          conf->offline_root = xstrdup(getenv("OFFLINE_ROOT"));
+	if (conf->offline_root)
+		sprintf_alloc(&etc_opkg_conf_pattern, "%s/etc/opkg/*.conf", conf->offline_root);
+	else {
+		const char *conf_file_dir = getenv("OPKG_CONF_DIR");
+		if (conf_file_dir == NULL)
+			conf_file_dir = OPKG_CONF_DEFAULT_CONF_FILE_DIR;
+			sprintf_alloc(&etc_opkg_conf_pattern, "%s/*.conf", conf_file_dir);
+	}
 
-     if (conf->conf_file) {
-	  struct stat stat_buf;
-	  err = stat(conf->conf_file, &stat_buf);
-	  if (err == 0)
-	       if (opkg_conf_parse_file(conf->conf_file,
-				    &conf->pkg_src_list, &tmp_dest_nv_pair_list)<0) {
-                   /* Memory leakage from opkg_conf_parse-file */
-                   return -1;
-               }
-     }
+	memset(&globbuf, 0, sizeof(globbuf));
+	if (glob(etc_opkg_conf_pattern, 0, NULL, &globbuf)) {
+		free(etc_opkg_conf_pattern);
+		globfree(&globbuf);
+		goto err1;
+	}
 
-     if (conf->offline_root)
-	  sprintf_alloc(&etc_opkg_conf_pattern, "%s/etc/opkg/*.conf", conf->offline_root);
-     else {
-	  const char *conf_file_dir = getenv("OPKG_CONF_DIR");
-	  if (conf_file_dir == NULL)
-		  conf_file_dir = OPKG_CONF_DEFAULT_CONF_FILE_DIR;
-	  sprintf_alloc(&etc_opkg_conf_pattern, "%s/*.conf", conf_file_dir);
-     }
-     memset(&globbuf, 0, sizeof(globbuf));
-     err = glob(etc_opkg_conf_pattern, 0, NULL, &globbuf);
-     free (etc_opkg_conf_pattern);
-     if (!err) {
-	  int i;
-	  for (i = 0; i < globbuf.gl_pathc; i++) {
-	       if (globbuf.gl_pathv[i]) 
-		    if (conf->conf_file &&
-				!strcmp(conf->conf_file, globbuf.gl_pathv[i]))
-			    continue;
-		    if ( opkg_conf_parse_file(globbuf.gl_pathv[i], 
-				         &conf->pkg_src_list, &tmp_dest_nv_pair_list)<0) {
-                        /* Memory leakage from opkg_conf_parse-file */
-                        return -1;
-	            }
-	  }
-     }
-     globfree(&globbuf);
+	free(etc_opkg_conf_pattern);
 
-     /* check for lock file */
-     if (conf->offline_root)
-       sprintf_alloc (&lock_file, "%s/%s/lock", conf->offline_root, OPKG_STATE_DIR_PREFIX);
-     else
-       sprintf_alloc (&lock_file, "%s/lock", OPKG_STATE_DIR_PREFIX);
+	for (i = 0; i < globbuf.gl_pathc; i++) {
+		if (globbuf.gl_pathv[i]) 
+			if (conf->conf_file &&
+					!strcmp(conf->conf_file, globbuf.gl_pathv[i]))
+				continue;
+		if ( opkg_conf_parse_file(globbuf.gl_pathv[i], 
+			&conf->pkg_src_list, &tmp_dest_nv_pair_list)<0) {
+			globfree(&globbuf);
+			goto err1;
+		}
+	}
 
-     lock_fd = creat(lock_file, S_IRUSR | S_IWUSR | S_IRGRP);
-     if (lock_fd == -1) {
-	     opkg_perror(ERROR, "Could not create lock file %s", lock_file);
-	     free(lock_file);
-	     return -1;
-     }
+	globfree(&globbuf);
 
-     if (lockf(lock_fd, F_TLOCK, (off_t)0) == -1) {
-	  opkg_perror(ERROR, "Could not lock %s", lock_file);
-	  free(lock_file);
-	  return -1;
-     }
+	if (conf->offline_root)
+		sprintf_alloc (&lock_file, "%s/%s/lock", conf->offline_root, OPKG_STATE_DIR_PREFIX);
+	else
+		sprintf_alloc (&lock_file, "%s/lock", OPKG_STATE_DIR_PREFIX);
 
-     free(lock_file);
+	lock_fd = creat(lock_file, S_IRUSR | S_IWUSR | S_IRGRP);
+	if (lock_fd == -1) {
+		opkg_perror(ERROR, "Could not create lock file %s", lock_file);
+		goto err2;
+	}
 
-     if (conf->tmp_dir)
-	  tmp_dir_base = conf->tmp_dir;
-     else 
-	  tmp_dir_base = getenv("TMPDIR");
-     sprintf_alloc(&tmp2, "%s/%s",
-		   tmp_dir_base ? tmp_dir_base : OPKG_CONF_DEFAULT_TMP_DIR_BASE,
-		   OPKG_CONF_TMP_DIR_SUFFIX);
-     if (conf->tmp_dir)
-	     free(conf->tmp_dir);
-     conf->tmp_dir = mkdtemp(tmp2);
-     if (conf->tmp_dir == NULL) {
-	  opkg_perror(ERROR, "Creating temp dir %s failed", tmp2);
-	  return -1;
-     }
+	if (lockf(lock_fd, F_TLOCK, (off_t)0) == -1) {
+		opkg_perror(ERROR, "Could not lock %s", lock_file);
+		goto err3;
+	}
 
-     pkg_hash_init();
-     hash_table_init("file-hash", &conf->file_hash, OPKG_CONF_DEFAULT_HASH_LEN);
-     hash_table_init("obs-file-hash", &conf->obs_file_hash, OPKG_CONF_DEFAULT_HASH_LEN/16);
+	if (conf->tmp_dir)
+		tmp_dir_base = conf->tmp_dir;
+	else
+		tmp_dir_base = getenv("TMPDIR");
 
-     if (conf->lists_dir == NULL)
-        conf->lists_dir = xstrdup(OPKG_CONF_LISTS_DIR);
+	sprintf_alloc(&tmp, "%s/%s",
+		tmp_dir_base ? tmp_dir_base : OPKG_CONF_DEFAULT_TMP_DIR_BASE,
+		OPKG_CONF_TMP_DIR_SUFFIX);
+	if (conf->tmp_dir)
+		free(conf->tmp_dir);
+	conf->tmp_dir = mkdtemp(tmp);
+	if (conf->tmp_dir == NULL) {
+		opkg_perror(ERROR, "Creating temp dir %s failed", tmp);
+		goto err4;
+	}
 
-     if (conf->offline_root) {
-            char *tmp;
-            sprintf_alloc(&tmp, "%s/%s", conf->offline_root, conf->lists_dir);
-            free(conf->lists_dir);
-            conf->lists_dir = tmp;
-     }
+	pkg_hash_init();
+	hash_table_init("file-hash", &conf->file_hash, OPKG_CONF_DEFAULT_HASH_LEN);
+	hash_table_init("obs-file-hash", &conf->obs_file_hash, OPKG_CONF_DEFAULT_HASH_LEN/16);
 
-     /* if no architectures were defined, then default all, noarch, and host architecture */
-     if (nv_pair_list_empty(&conf->arch_list)) {
-	  nv_pair_list_append(&conf->arch_list, "all", "1");
-	  nv_pair_list_append(&conf->arch_list, "noarch", "1");
-	  nv_pair_list_append(&conf->arch_list, HOST_CPU_STR, "10");
-     }
+	if (conf->lists_dir == NULL)
+		conf->lists_dir = xstrdup(OPKG_CONF_LISTS_DIR);
 
-     /* Even if there is no conf file, we'll need at least one dest. */
-     if (nv_pair_list_empty(&tmp_dest_nv_pair_list)) {
-	  nv_pair_list_append(&tmp_dest_nv_pair_list,
-			      OPKG_CONF_DEFAULT_DEST_NAME,
-			      OPKG_CONF_DEFAULT_DEST_ROOT_DIR);
-     }
+	if (conf->offline_root) {
+		sprintf_alloc(&tmp, "%s/%s", conf->offline_root, conf->lists_dir);
+		free(conf->lists_dir);
+		conf->lists_dir = tmp;
+	}
 
-     err = resolve_pkg_dest_list(&tmp_dest_nv_pair_list);
-     nv_pair_list_deinit(&tmp_dest_nv_pair_list);
+	/* if no architectures were defined, then default all, noarch, and host architecture */
+	if (nv_pair_list_empty(&conf->arch_list)) {
+		nv_pair_list_append(&conf->arch_list, "all", "1");
+		nv_pair_list_append(&conf->arch_list, "noarch", "1");
+		nv_pair_list_append(&conf->arch_list, HOST_CPU_STR, "10");
+	}
 
-     if (err)
+	/* Even if there is no conf file, we'll need at least one dest. */
+	if (nv_pair_list_empty(&tmp_dest_nv_pair_list)) {
+	nv_pair_list_append(&tmp_dest_nv_pair_list,
+			OPKG_CONF_DEFAULT_DEST_NAME,
+			OPKG_CONF_DEFAULT_DEST_ROOT_DIR);
+	}
+
+	if (resolve_pkg_dest_list(&tmp_dest_nv_pair_list))
+		goto err5;
+
+	nv_pair_list_deinit(&tmp_dest_nv_pair_list);
+
+	return 0;
+
+
+err5:
+	free(conf->lists_dir);
+
+	pkg_hash_deinit();
+	hash_table_deinit(&conf->file_hash);
+	hash_table_deinit(&conf->obs_file_hash);
+
+	if (rmdir(conf->tmp_dir) == -1)
+		opkg_perror(ERROR, "Couldn't remove dir %s", conf->tmp_dir);
+err4:
+	if (lockf(lock_fd, F_ULOCK, (off_t)0) == -1)
+		opkg_perror(ERROR, "Couldn't unlock %s", lock_file);
+err3:
+	if (close(lock_fd) == -1)
+		opkg_perror(ERROR, "Couldn't close descriptor %d (%s)",
+				lock_fd, lock_file);
+	if (unlink(lock_file) == -1)
+		opkg_perror(ERROR, "Couldn't unlink %s", lock_file);
+err2:
+	free(lock_file);
+err1:
+	pkg_src_list_deinit(&conf->pkg_src_list);
+	pkg_dest_list_deinit(&conf->pkg_dest_list);
+	nv_pair_list_deinit(&conf->arch_list);
+
+	for (i=0; options[i].name; i++) {
+		if (options[i].type == OPKG_OPT_TYPE_STRING) {
+			tmp_val = (char **)options[i].value;
+			if (*tmp_val) {
+				free(*tmp_val);
+				*tmp_val = NULL;
+			}
+		}
+	}
+err0:
+	nv_pair_list_deinit(&tmp_dest_nv_pair_list);
+	if (conf->dest_str)
+		free(conf->dest_str);
+	if (conf->conf_file)
+		free(conf->conf_file);
+
 	return -1;
-
-     return 0;
 }
 
 void
@@ -552,10 +610,15 @@ opkg_conf_deinit(void)
 	hash_table_deinit(&conf->file_hash);
 	hash_table_deinit(&conf->obs_file_hash);
 
-	/* lockf may be defined with warn_unused_result */
-	if (lockf(lock_fd, F_ULOCK, (off_t)0) != 0) {
-		opkg_perror(ERROR, "unlock failed");
-	}
+	if (lockf(lock_fd, F_ULOCK, (off_t)0) == -1)
+		opkg_perror(ERROR, "Couldn't unlock %s", lock_file);
 
-	close(lock_fd);
+	if (close(lock_fd) == -1)
+		opkg_perror(ERROR, "Couldn't close descriptor %d (%s)",
+				lock_fd, lock_file);
+
+	if (unlink(lock_file) == -1)
+		opkg_perror(ERROR, "Couldn't unlink %s", lock_file);
+
+	free(lock_file);
 }
